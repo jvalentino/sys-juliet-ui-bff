3dfe2f67a286d6272e8d5cd4dabd41f4b70c8f0c
John Valentino <jvalentino2@gmail.com>
2023-03-14 12:49:12 -0500

    Added swagger codegen

--- src/main/groovy/com/github/jvalentino/juliet/rest/BffRest.groovy
+++ src/main/groovy/com/github/jvalentino/juliet/rest/BffRest.groovy
@@ -1,6 +1,7 @@
 package com.github.jvalentino.juliet.rest
 
-import com.github.jvalentino.juliet.dto.ResultDto
+import com.github.jvalentino.juliet.doc.api.DocRestApi
+import com.github.jvalentino.juliet.doc.model.DocListDto
 import groovy.transform.CompileDynamic
 import groovy.util.logging.Slf4j
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
@@ -17,8 +18,11 @@ class BffRest {
 
     @GetMapping('/')
     @CircuitBreaker(name = 'Index')
-    ResultDto index() {
-        new ResultDto()
+    DocListDto index() {
+        DocRestApi api = new DocRestApi()
+        api.apiClient.apiKey = '123'
+        DocListDto result = api.dashboard()
+        result
     }
 
 }
