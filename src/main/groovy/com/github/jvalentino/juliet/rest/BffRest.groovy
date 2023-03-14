67f508f8afc3ce2acd1b8c96bb5cf3c4ca7a826a
John Valentino <jvalentino2@gmail.com>
2023-03-14 10:20:22 -0500

    got swagger working

---
+++ src/main/groovy/com/github/jvalentino/juliet/rest/BffRest.groovy
@@ -0,0 +1,24 @@
+package com.github.jvalentino.juliet.rest
+
+import com.github.jvalentino.juliet.dto.ResultDto
+import groovy.transform.CompileDynamic
+import groovy.util.logging.Slf4j
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Generic services for the BFF
+ */
+@CompileDynamic
+@Slf4j
+@RestController
+class BffRest {
+
+    @GetMapping('/')
+    @CircuitBreaker(name = 'Index')
+    ResultDto index() {
+        new ResultDto()
+    }
+
+}
