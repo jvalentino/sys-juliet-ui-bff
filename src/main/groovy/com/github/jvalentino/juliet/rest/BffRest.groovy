a4dd3d320f92cb7a5d445a5374c118ad0341d9ac
John Valentino <jvalentino2@gmail.com>
2023-03-14 15:23:55 -0500

    Got login working across services

--- src/main/groovy/com/github/jvalentino/juliet/rest/BffRest.groovy
+++ src/main/groovy/com/github/jvalentino/juliet/rest/BffRest.groovy
@@ -2,13 +2,21 @@ package com.github.jvalentino.juliet.rest
 
 import com.github.jvalentino.juliet.dto.DashboardDto
 import com.github.jvalentino.juliet.dto.HomeDto
+import com.github.jvalentino.juliet.dto.LoginDto
+import com.github.jvalentino.juliet.service.AuthService
 import com.github.jvalentino.juliet.service.BffService
+import com.github.jvalentino.juliet.user.model.UserDto
 import groovy.transform.CompileDynamic
 import groovy.util.logging.Slf4j
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+
+import javax.servlet.http.HttpSession
 
 /**
  * Generic services for the BFF
@@ -20,6 +28,12 @@ class BffRest {
 
     @Autowired
     BffService bffService
+
+    @Autowired
+    AuthService authService
+
+    @Autowired
+    AuthenticationManager authenticationManager
 
     @GetMapping('/')
     @CircuitBreaker(name = 'Index')
@@ -34,6 +48,12 @@ class BffRest {
         result.with {
             documents = bffService.allDocs()
         }
+        result
+    }
+
+    @PostMapping('/custom-login')
+    LoginDto login(@RequestBody UserDto user, HttpSession session) {
+        LoginDto result = authService.login(user, authenticationManager, session)
         result
     }
 
