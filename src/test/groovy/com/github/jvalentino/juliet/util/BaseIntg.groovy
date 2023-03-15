d842f620d9931ab34486ce57d8f1ca6f81aac4b4
John Valentino <jvalentino2@gmail.com>
2023-03-15 14:46:42 -0500

    Test coverage

--- src/test/groovy/com/github/jvalentino/juliet/util/BaseIntg.groovy
+++ src/test/groovy/com/github/jvalentino/juliet/util/BaseIntg.groovy
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
@@ -28,6 +30,16 @@ abstract class BaseIntg extends Specification {
     Object toObject(MvcResult response, Class clazz) {
         String json = response.getResponse().getContentAsString()
         new ObjectMapper().readValue(json, clazz)
+    }
+
+    void mockAdminLoggedIn() {
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken('1', null)
+        SecurityContextHolder.getContext().setAuthentication(authentication)
+    }
+
+    String toJson(Object obj) {
+        new ObjectMapper().writeValueAsString(obj)
     }
 
 }
